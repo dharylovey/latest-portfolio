f369b7b9f9aaea7cb7350bd4465d5b16440e6ae5
Dharyl Carry S. Almora <37977733+dharylovey@users.noreply.github.com>
2024-04-19 20:52:30 +0800

    fix nav themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <div id="about" className="min-h-screen flex items-center justify-center text-center">about</div>
+      <div id="about" className="min-h-screen flex items-center justify-center text-center">about section</div>
       <div id="experience"className="min-h-screen flex items-center justify-center text-center">experience</div>
       <div id="myskills" className="min-h-screen flex items-center justify-center text-center">myskills</div>
       <div id="project" className="min-h-screen flex items-center justify-center text-center">project</div>
